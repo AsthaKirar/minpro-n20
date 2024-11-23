2a6dfa4e0159ec0f11dd1ac2fcf117f5e9e4dee2
AsthaKirar <asthap602@gmail.com>
2024-11-23 22:02:05 +0530

    commit

--- app.js
+++ app.js
@@ -5,55 +5,89 @@ const userModel = require("./models/user.model");
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
+const flash = require("connect-flash")
+const expressSession = require("express-session")
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: "hjagshkncbhjakskzbchkj"
+}));
+app.use(flash());
+
 
 
 // Define the route for the root path
 app.get("/", function(req, res) {
-    res.render("login");
+    res.render("welcome");
+});
+app.get("/profile", isLoggedIn,async function (req, res) {
+    let user = await userModel.findOne({ username: req.user.username });
+
+    res.render("profile",{user});
 });
 
+
 app.get("/register", function(req, res) {
-    res.render("register");
+    res.render("register", { error: req.flash("error")[0] });
+
 });
-app.post("/register", function(req, res) {
-    const { username, email, password } = req.body
+app.post("/register",  async function(req, res) {
+    let { username,  password } = req.body
+    let user = await userModel.findOne({ username });
+    if (user) {
+        req.flash("error", "account already exists, please login.");
+        return res.redirect("/register");
+    }
+
     bcrypt.genSalt(10,function(err,salt){
         console.log(salt)
         bcrypt.hash(password,salt,async function(err,hash){
             console.log(hash)
-            const user = await userModel.create({
+             await userModel.create({
                 username,
-                email,
                 password:hash,
             })
-            const token = jwt.sign({ email },"screte");
+            const token = jwt.sign({ username },"screte");
             res.cookie("token",token)
-            res.send(user);
+            res.redirect("/profile");
+
+            
         })
     })
     
 });
 app.get("/login", function(req, res) {
-    res.render("login");
+    res.render("login", { error: req.flash("error")[0] });
+
 });
 
 
 app.post("/login",async function(req, res) {
-    let { username, email, password } = req.body
+    let { username,  password } = req.body
     let user = await userModel.findOne({ username });
-    if(!user) return res.send("inccorect username or password");
+    if (!user) {
+        req.flash("error", "username or password is incorrect.");
+        return res.redirect("/login");
+    }
     bcrypt.compare(password,user.password, function(err,result){
         if(result){
-            let token = jwt.sign({email},screte)
+            let token = jwt.sign({username},screte)
             res.cookie("token",token)
+            res.redirect("/profile");
+
             res.send(loggedin);
         }
+        else {
+            req.flash("error", "username or password is incorrect.")
+            res.redirect("/login");
+        }
+
     })
 
 });
--- models/user.model.js
+++ models/user.model.js
@@ -5,13 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+        // trim: true,
     },
     password: {
         type: String,
